cbb92e223dff37c83b684ac0aedd02cee65f30a4
Ethan Glasser-Camp <ethan@betacantrips.com>
2014-02-06 10:35:01 -0500

    this might be the best way to add form handling to submit moves

--- Handler/TicTacToeGame.hs
+++ Handler/TicTacToeGame.hs
@@ -2,8 +2,9 @@
 module Handler.TicTacToeGame where
 
 import Import
-import Data.Text as T
-import Data.Maybe
+import qualified Data.Text as T
+import Yesod.Auth
+import Data.List.Split
 
 toHuman :: KeyBackend backend entity -> T.Text
 toHuman = toPathPiece . unKey
@@ -14,6 +15,8 @@ entityPlayerName = maybe (T.pack "Unknown Player") (userIdent . entityVal)
 maybePlayerName :: Maybe User -> Text
 maybePlayerName = maybe (T.pack "Unknown Player") userIdent
 
+titleForGameId gameId = setTitle $ toHtml $ (T.append . T.pack) "TicTacToe - Game " $ toHuman gameId
+
 getTicTacToeListR :: Handler Html
 getTicTacToeListR = do
     defaultLayout $ do
@@ -22,8 +25,31 @@ getTicTacToeListR = do
 
 postTicTacToeGameR :: TicTacToeGameId -> Handler Html
 postTicTacToeGameR gameId = do
+    defaultLayout $ do
+        titleForGameId gameId
+        [whamlet|<h1>You made a move:|]
     -- some stuff
-    redirect (TicTacToeGameR gameId)
+    -- redirect (TicTacToeGameR gameId)
+
+
+boardField :: [[Either (Int, Int) TicTacToeSpace]] -> Field Handler (Int, Int)
+boardField gameBoard = Field
+    { fieldParse = \rawVals _fileVals ->
+        case rawVals of
+            [space] -> let chunks = splitOn "," (T.unpack space) in
+                if length chunks == 2
+                then
+                    let firstChunk = chunks !! 0
+                        secondChunk = chunks !! 1
+                    in return $ Right $ Just (read firstChunk, read secondChunk)
+                else
+                    return $ Left $ SomeMessage $ T.append "Invalid space value " space
+            [] -> return $ Right Nothing
+            _ -> return $ Left $ "Didn't get any space"
+    , fieldView = \idAttr nameAttr otherAttrs eResult isReq ->
+        $(widgetFile "tictactoe/board-form")
+    , fieldEnctype = UrlEncoded
+    }
 
 getTicTacToeGameR :: TicTacToeGameId -> Handler Html
 getTicTacToeGameR gameId = do
@@ -31,6 +57,9 @@ getTicTacToeGameR gameId = do
     gameBoard <- runDB $ board gameId
     maybePlayer1 <- runDB $ get (ticTacToeGamePlayer1 game)
     maybePlayer2 <- runDB $ get (ticTacToeGamePlayer2 game)
+    let nextPlayerId = ticTacToeGameNextPlayerId game
+    viewer <- maybeAuthId
+    ((res, boardWidget), enctype) <- runFormPost $ renderDivs $ areq (boardField gameBoard) "Make a move!" Nothing
     defaultLayout $ do
-        setTitle $ toHtml $ (T.append . T.pack) "TicTacToe - Game " $ toHuman gameId
+        titleForGameId gameId
         $(widgetFile "tictactoe/detail")
--- Model.hs
+++ Model.hs
@@ -19,9 +19,19 @@ share [mkPersist sqlOnlySettings, mkMigrate "migrateAll"]
 hasCoords :: Int -> Int -> TicTacToeSpace -> Bool
 hasCoords x y (TicTacToeSpace x0 y0 _ _) = x == x0 && y == y0
 
+-- I have no idea whether this is even correct, but trying it gets me a circular import error.
+-- board :: Key TicTacToeGame -> YesodDB App [[Either (Int,Int) TicTacToeSpace]]
 board gameId = do
     spaces <- selectList [TicTacToeSpaceGame ==. gameId] []
-    return [[find (hasCoords x y) $ map entityVal spaces | x <- [0..2]] | y <- [0..2]]
+    return [[maybe (Left (x, y)) Right $ find (hasCoords x y) $ map entityVal spaces
+             | x <- [0..2]] | y <- [0..2]]
 
 ticTacToeSpaceValueHtml :: TicTacToeSpace -> Html
 ticTacToeSpaceValueHtml space = toHtml $ pack $ if ticTacToeSpaceValue space == PlayerX then "X" else "O"
+
+getPlayerForSide :: Player -> TicTacToeGame -> UserId
+getPlayerForSide PlayerX = ticTacToeGamePlayer1
+getPlayerForSide PlayerO = ticTacToeGamePlayer2
+
+ticTacToeGameNextPlayerId :: TicTacToeGame -> Key User
+ticTacToeGameNextPlayerId game = getPlayerForSide (ticTacToeGameNextPlayer game) game
